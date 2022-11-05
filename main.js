791f995a5e09011817fb48d0bced872a7042bb97
Andre Richardi <a.richardi@gmail.com>
2022-11-05 13:54:53 -0300

    menu icon and about page done

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const { app, BrowserWindow, dialog, ipcMain} = require('electron');
+const { app, BrowserWindow, dialog, ipcMain, Menu} = require('electron');
 const path = require('path')
 const fs = require ('fs')
 
-process.env.NODE_ENV = 'devlopment';
 
-const IS_DEV = process.env.NODE_ENV !== 'development';
+process.env.NODE_ENV = 'production';
+
+const IS_DEV = process.env.NODE_ENV !== 'production';
 
 //The app main window creation
 function createMainWindow(){
     const mainWindow = new BrowserWindow({
         title: 'docRenamer',
+        icon: path.join(__dirname, './public/icon.ico') ,
         width: IS_DEV ? 1100: 600,
         height: 720,
         webPreferences: {
@@ -26,12 +28,48 @@ function createMainWindow(){
     mainWindow.loadFile(path.join(__dirname, './App/index.html'));
 }
 
+//create about window
+function createAboutWindow(){
+    const aboutWindow = new BrowserWindow({
+        title: 'About',
+        icon: path.join(__dirname, './public/icon.ico') ,
+        width: 400,
+        height: 400,
+    })
+
+    aboutWindow.loadFile(path.join(__dirname,'./App/about.html'))
+}
+
+const menuTemplate = [
+    {
+        label: 'file',
+        submenu: [
+            {
+                label: 'LoadFiles',
+                click: () => filesSelectionHandler(),
+            },
+            {
+                label: 'About',
+                click: () => createAboutWindow(),
+
+            },
+            {
+                label: 'Exit',
+                click: () => app.quit(),
+            },
+        ],
+    }
+]
+
+
 app.whenReady().then( () => {
     ipcMain.handle('fileSelect', filesSelectionHandler)
     ipcMain.handle('fileRename', (event, oldName, newName) => {
         filesRenamer(oldName, newName)
     })
     createMainWindow();
+    const menu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(menu)
 
     app.on('window-all-closed', () => {
         if(process.platform !== "darwin"){
